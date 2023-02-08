d47fda371d68bced749138e3e10e1164240af14d
Ricardo <ricardomesquita12@hotmail.com>
2023-02-08 13:46:19 -0300

    aula 162

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { OfertasService } from '../ofertas.service';
+import { OfertasService } from '../ofertas.services'
 import { Oferta } from '../shared/oferta.model'
 @Component({
   selector: 'app-home',
@@ -10,20 +10,22 @@ import { Oferta } from '../shared/oferta.model'
 })
 export class HomeComponent implements OnInit {
 
-  public ofertas!: Oferta[]
+  public ofertas: Oferta[]
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
-   // this.ofertas = this.ofertasService.getOfertas()
-   // console.log(this.ofertas)
-   
-   this.ofertasService.getOfertas2()
-    .then((ofertas: Oferta[])=>{
-      this.ofertas = ofertas
-    })
-    .catch((param:any)=>{
-      console.log(param)
-    })
+    // this.ofertas = this.ofertasService.getOfertas()
+    // console.log(this.ofertas)
+
+    this.ofertasService.getOfertas()
+      .then((ofertas: Oferta[]) => {
+        this.ofertas = ofertas
+      })
+      .catch((param: any) => { 
+        console.log(param) 
+      })
+
+
   }
 
 }
